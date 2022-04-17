a1c27fe303947d821dec5d5bb348f03f2e6f94c9
MagnetarPipa <GITHUB you@example.com>
2022-04-17 21:17:19 +0300

    Task 02 is underway

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -60,9 +60,80 @@ class Product {
      */
     public void PrintInformation() {
 
-        System.out.println("Количество:" + getAmount() + " Цена" + getPrice() + " Год изготовления:" + getYearOfProduction() + " Производитель:" + getCompanyName());
+        System.out.println("Количество:" + getAmount() + " Цена:" + getPrice() + " Год изготовления:" + getYearOfProduction() + " Производитель:" + getCompanyName());
     }
 
+}
+
+/**
+ * Фамилия
+ * Дата рождения
+ * Амплуа
+ * Количество игр
+ * Количество забитых мячей
+ * Место рождения
+ */
+class Footballer {
+
+    private String surname = "";
+
+    public void setSurname(String str) {
+        this.surname = str;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    private int birthdate = 0;
+
+    public void setBirthDate(int b_date) {
+        this.birthdate = b_date;
+    }
+
+    public int getBirthDate() {
+        return birthdate;
+    }
+
+    private String role = "";
+
+    public void setRole(String str) {
+        this.role = str;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    private int amount_of_games = 0;
+
+    public void setAmount_of_games(int amount) {
+        this.amount_of_games = amount;
+    }
+
+    public int getAmount_of_games() {
+        return amount_of_games;
+    }
+
+    private float amount_of_goals = 0;
+
+    public void setAmount_of_goals(float goals) {
+        this.amount_of_goals = goals;
+    }
+
+    public float getAmount_of_goals() {
+        return amount_of_goals;
+    }
+
+    private String birth_place = "";
+
+    public void setBirth_place(String place) {
+        this.birth_place = place;
+    }
+
+    public int getBirthdate() {
+        return birthdate;
+    }
 }
 
 
@@ -73,7 +144,7 @@ public class Main {
     public static void main(String[] args) {
 
         task01();
-
+        task02();
     }
 
     /**
@@ -89,8 +160,8 @@ public class Main {
         company_names[0] = "Muttela";
         company_names[1] = "Koka Cola";
         company_names[2] = "Odidas";
-        company_names[3] = "Cow King";
-        company_names[4] = "Windows Donald's";
+        company_names[3] = "TerraMars inc.";
+        company_names[4] = "Liandri Corporations";
 
         int count = 0;
         for (int i = 0; i < products.length; i++) {
@@ -98,7 +169,7 @@ public class Main {
             products[i] = new Product();
             products[i].setCompanyName(company_names[(int) (Math.random() * 5)]);
             products[i].setPrice(1 + (int) (Math.random() * 3000));
-            products[i].setYear0fProduction(1999 + (int) (Math.random() * 2022));
+            products[i].setYear0fProduction(1999 + (int) (Math.random() * 2026));
 
             count++;
 
@@ -120,6 +191,26 @@ public class Main {
         }
         System.out.print("Самый дорогой товар на складе и информация о нем: ");
         products[maxIndex].PrintInformation();
+
+    }
+
+    /**
+     * Вывести сведения о футболистах, старших 20-ти лет и
+     * забивающих за игру не менее 0,4 мяча.
+     */
+    private static void task02() {
+
+        Footballer[] footballers = new Footballer[5];
+        String[] names = new String[5];//Взял наиболее часто встречающиеся
+        names[0]="Liam";
+        names[1]="Noah";
+        names[2]="Oliver";
+        names[3]="Elijah";
+        names[4]="William";
+
+
+
+
 
     }
 
